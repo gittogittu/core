b9e52841f11c4166969312f468e72cb1c84ce022
Dann Brothers <spatan74@mail.com>
2018-04-17 14:48:55 +0200

    Update classes.js

--- classes.js
+++ classes.js
@@ -1,7 +1,12 @@
-module.exports = function() {
-
+module.exports = {
+	MakeServer: function(name, main) {
+		return new Server(name, main);
+	}
 }
 
 class Server {
-
+	constructor(name, main) {
+		this.name = name;
+		this.main = main;
+	}
 }
